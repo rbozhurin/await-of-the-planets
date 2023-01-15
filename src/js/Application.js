7de38e18c55587c8d086fc79e9abfc05d4e90a13
Rayko Bozhurin <rbozhurin@gmail.com>
2023-01-15 17:51:17 +0200

    asas

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,5 +1,4 @@
 import EventEmitter from "eventemitter3";
-import image from "../images/planet.svg";
 
 export default class Application extends EventEmitter {
   static get events() {
@@ -10,38 +9,58 @@ export default class Application extends EventEmitter {
 
   constructor() {
     super();
+    this._loading = document.getElementById("loading");
 
-    const box = document.createElement("div");
-    box.classList.add("box");
-    box.innerHTML = this._render({
-      name: "Placeholder",
-      terrain: "placeholder",
-      population: 0,
+    const button = document.querySelector(".button");
+    button.addEventListener("click", () => {
+      alert("💣");
     });
-
-    document.body.querySelector(".main").appendChild(box);
 
     this.emit(Application.events.READY);
   }
 
-  _render({ name, terrain, population }) {
-    return `
-<article class="media">
-  <div class="media-left">
-    <figure class="image is-64x64">
-      <img src="${image}" alt="planet">
-    </figure>
-  </div>
-  <div class="media-content">
-    <div class="content">
-    <h4>${name}</h4>
-      <p>
-        <span class="tag">${terrain}</span> <span class="tag">${population}</span>
-        <br>
-      </p>
-    </div>
-  </div>
-</article>
-    `;
+  async _load() {
+    this._startLoading();
+    let nextPage = "/planets";
+    let planets = [];
+    while (nextPage) {
+      let response = await fetch(nextPage);
+      let data = await response.json();
+      planets = planets.concat(data.results);
+      nextPage = data.next;
+    }
+    this._stopLoading();
+    return planets;
+  }
+
+  _create(planets) {
+    planets.forEach((planet) => {
+      let planetDiv = document.createElement("div");
+      planetDiv.classList.add("planet-box");
+      planetDiv.innerHTML = `
+        <h2>${planet.name}</h2>
+        <p>Population: ${planet.population}</p>
+      `;
+      planetContainer.appendChild(planetDiv);
+    });
+  }
+
+  _startLoading() {
+    this._loading.style.display = "block";
+  }
+
+  _stopLoading() {
+    this._loading.style.display = "none";
   }
 }
+
+const app = new Application();
+
+app
+  ._load()
+  .then((planets) => {
+    app._create(planets);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
